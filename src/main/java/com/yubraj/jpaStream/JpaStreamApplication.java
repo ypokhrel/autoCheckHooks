380ad909364d3bdffaf5f3ce8624b5abf3e449f4
Yubraj Pokharel <yubraj.pokhrel@gainwelltechnologies.com>
2023-01-25 10:44:31 -0600

    initialize

---
+++ src/main/java/com/yubraj/jpaStream/JpaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.yubraj.jpaStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStreamApplication.class, args);
+	}
+
+}
